0c7e7132654413371b2db0e975c70e404ff25c10
Ronni-FSTK <u202224619@upc.edu.pe>
2025-05-14 13:49:34 -0500

    add page postulante

--- src/app/modules/postulante/components/LayoutPostulante.component.vue
+++ src/app/modules/postulante/components/LayoutPostulante.component.vue
@@ -3,7 +3,7 @@ import HeaderComponent from "../../../shared/Header.component.vue";
 import PostulanteSidebar from "../../../shared/PostulanteSidebar.vue";
 
 export default {
-  name: "Layout.component",
+  name: "LayoutPostulante.component",
   components: {
     HeaderComponent,
     PostulanteSidebar,
@@ -12,7 +12,7 @@ export default {
 </script>
 
 <template>
-  <div class="layout-wrapper">
+  <div>
     <HeaderComponent />
     <div class="dashboard-container">
       <PostulanteSidebar />
@@ -23,6 +23,18 @@ export default {
   </div>
 </template>
 
+
 <style scoped>
 
+.dashboard-container {
+  display: flex;
+  flex-grow: 1;
+}
+
+.content-area {
+  flex-grow: 1;
+  padding: 20px;
+  background-color: #ffffff;
+  overflow-y: auto;
+}
 </style>
