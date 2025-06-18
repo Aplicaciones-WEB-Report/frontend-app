54d352ae3fa1eea0e029552da96014379e861ba3
SrtaYeis <jasminurrutia30@gmail.com>
2025-06-18 01:37:55 -0500

    solucion de error en i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -3,6 +3,7 @@ import Español from './Locales/es.json';
 import {createI18n} from "vue-i18n";
 
 const i18n = createI18n({
+    legacy: false,
     locale: "Español",
     fallbackLocale: "Ingles",
     globalInjection: true,
